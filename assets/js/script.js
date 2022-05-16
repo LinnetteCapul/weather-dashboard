1a4f3df430bb3b6c5d45af8bf1eab2bda5c4cb17
LinnetteCapul <linnette.capul@gmail.com>
2022-05-16 16:12:43 -0700

    added some bootstrap styling and stated vars in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,40 @@
 var APIKey = "11e835335ab747fc1ec7eac03b124031"
 
+var inputCity = $("#input-city");
+var searchBtn = $("#search-btn");
+var pastSearches = $("#past-searches");
+
+var currentWeather = $("#current-weather");
+var cityName = $("#city-name");
+var temp = $("#temp");
+var wind = $("#wind");
+var humidity = $("#humidity");
+var uvIndex = $("#uv-index");
+
+var fiveDays = $("#five-days");
+
+var oneDay = $("#one-day");
+var oneTemp = $("#one-day-temp");
+var oneWind = $("#one-day-wind");
+var oneHumidity = $("#one-day-humidity");
+
+var twoDay = $("#two-day");
+var twoTemp = $("#two-day-temp");
+var twoWind = $("#two-day-wind");
+var twoHumidity = $("#two-day-humidity");
+
+var threeDay = $("#three-day");
+var threeTemp = $("#three-day-temp");
+var threeWind = $("#three-day-wind");
+var threeHumidity = $("#three-day-humidity");
+
+var fourDay = $("#four-day");
+var fourTemp = $("#four-day-temp");
+var fourWind = $("#four-day-wind");
+var fourHumidity = $("#four-day-humidity");
+
+var fiveDay = $("#five-day");
+var fiveTemp = $("#five-day-temp");
+var fiveWind = $("#five-day-wind");
+var fiveHumidity = $("#five-day-humidity");
 
